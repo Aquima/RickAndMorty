6737ebfc41e023eb0d818191ad9468f26434501e
Raul S. Quispe Mamani <raul.quispe@live.com>
2024-05-15 09:23:08 -0500

    Refactor CharacterListView into modular components for better readability and maintainability.

--- RickAndMorty/Scenes/CharacterList/View/CharacterListView.swift
+++ RickAndMorty/Scenes/CharacterList/View/CharacterListView.swift
@@ -9,44 +9,65 @@ import SwiftUI
 
 struct CharacterListView: View {
     @ObservedObject private(set) var viewModel: CharacterListViewModel
-    @State var searchTerm: String = ""
+    @State private var searchTerm: String = ""
+
     init() {
         _viewModel = ObservedObject(wrappedValue: CharacterListViewModel())
     }
 
     var body: some View {
         NavigationStack {
-            List { ForEach(viewModel.previewCharacters, id: \.id) { character in
-                    NavigationLink {
-                        CharacterDetailView(currentCharacter: .constant(character))
-                    } label: {
-                        CharacterItemView(character: .constant(character))
-                            .background(.clear)
-                    }.listRowSeparator(.hidden)
+            CharactersList(viewModel: viewModel, searchTerm: $searchTerm)
+                .navigationTitle(viewModel.titleNavigation)
+                .navigationBarTitleDisplayMode(.inline)
+                .searchable(text: $searchTerm, placement: .navigationBarDrawer(displayMode: .always))
+                .onChange(of: searchTerm) { oldValue, newValue in
+                    handleSearchTermChange(oldValue: oldValue, newValue: newValue)
                 }
-                CharacterLastRowView(paginationState: viewModel.paginationState,
-                                     loadMoreItems: viewModel.loadMoreItems)
-                    .listRowSeparator(.hidden)
-                    .id("CharacterLastRowView")
-            }
-            .listStyle(.plain)
-            .navigationTitle(viewModel.titleNavigation)
-            .font(.subheadline)
-            .padding([.leading, .trailing], 0)
-            .navigationBarTitleDisplayMode(.inline)
-            .id("CharactersList")
-            .searchable(text: $searchTerm, placement: .navigationBarDrawer(displayMode: .always))
-            .onChange(of: searchTerm) { oldValue, newValue in
-                if oldValue.count > 1 {
-                    viewModel.searchName = newValue
-                    viewModel.loadMoreItems()
-                } else {
-                    viewModel.searchName = ""
-                }
-            }
         }
     }
 
+    private func handleSearchTermChange(oldValue: String, newValue: String) {
+        if oldValue.count > 1 {
+            viewModel.searchName = newValue
+            viewModel.loadMoreItems()
+        } else {
+            viewModel.searchName = ""
+        }
+    }
+}
+
+struct CharactersList: View {
+    @ObservedObject var viewModel: CharacterListViewModel
+    @Binding var searchTerm: String
+
+    var body: some View {
+        List {
+            characterItems
+            paginationView
+        }
+        .listStyle(.plain)
+        .padding([.leading, .trailing], 0)
+        .id("CharactersList")
+    }
+
+    private var characterItems: some View {
+        ForEach(viewModel.previewCharacters, id: \.id) { character in
+            NavigationLink {
+                CharacterDetailView(currentCharacter: .constant(character))
+            } label: {
+                CharacterItemView(character: .constant(character))
+                    .background(.clear)
+            }
+            .listRowSeparator(.hidden)
+        }
+    }
+
+    private var paginationView: some View {
+        CharacterLastRowView(paginationState: viewModel.paginationState, loadMoreItems: viewModel.loadMoreItems)
+            .listRowSeparator(.hidden)
+            .id("CharacterLastRowView")
+    }
 }
 
 #Preview {
